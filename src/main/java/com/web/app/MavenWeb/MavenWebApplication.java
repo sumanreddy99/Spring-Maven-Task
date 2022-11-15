e8e60945168605496b814667c8147163c7cabaa0
madhadi suman reddy <m.suman.reddy@live.com>
2022-11-15 07:36:05 +0530

    first commit

---
+++ src/main/java/com/web/app/MavenWeb/MavenWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.app.MavenWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/app/MavenWeb/MavenWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.app.MavenWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
